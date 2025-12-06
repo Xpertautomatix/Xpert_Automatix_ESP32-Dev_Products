eb5c7d16dcbedfecb58eb21fb76fed5417a38fe6
Xpertautomatix <xpertautomatix@gmail.com>
2025-12-06 11:33:59 +0530

    Hotspot_Generation
    Hotspot_Generation

---
+++ XA_ESP32 DEVELOPMENT BOARD/Wi-Fi/Hotspot_Generation/Hotspot_Generation.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "Vignesh_KPR";  // Hotspot name
+const char *password = "12345678";    // Hotspot password (min. 8 characters)
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Initialize Wi-Fi access point
+  WiFi.softAP(ssid, password);
+
+  // Print the IP address
+  Serial.print("Access Point Created. IP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void loop() {
+  // Your code here (e.g., serve web pages, handle requests)
+}
